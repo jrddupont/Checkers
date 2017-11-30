40e0cd4ba491149834f8f2fec09e3c77d3b3978b
doominabox1 <jrddupont@gmail.com>
2017-11-30 12:11:19 -0600

    Added GUI junk

--- src/client/DumbAIPlayer.java
+++ src/client/DumbAIPlayer.java
@@ -5,6 +5,10 @@ import java.util.Random;
 
 import util.Board;
 
+/**
+ * Picks random possible moves (Sometimes)
+ *
+ */
 public class DumbAIPlayer extends AIPlayer{
 	int playerNumber = 0;
 	Random rand = new Random();
@@ -13,6 +17,7 @@ public class DumbAIPlayer extends AIPlayer{
 		playerNumber = pn;
 	}
 	
+	// Picks a random move
 	@Override
 	public Board getMove(Board board) {
 		ArrayList<Board> moves = board.getNextBoards(playerNumber);
